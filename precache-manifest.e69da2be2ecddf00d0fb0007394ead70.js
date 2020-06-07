854533cddfa476fe7ca1d46948f80c8ac75f5664
Chunhao Zheng <chz062009@gmail.com>
2020-06-07 18:08:27 -0400

    Updates

---
+++ precache-manifest.e69da2be2ecddf00d0fb0007394ead70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa253c4201dbdd7204e3a217eee7b9f6",
+    "url": "/bank-of-react/index.html"
+  },
+  {
+    "revision": "dda2a52351b91e752bb6",
+    "url": "/bank-of-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cd2508791ec33f5bba05",
+    "url": "/bank-of-react/static/js/2.429b02db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bank-of-react/static/js/2.429b02db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda2a52351b91e752bb6",
+    "url": "/bank-of-react/static/js/main.eb60c3ff.chunk.js"
+  },
+  {
+    "revision": "4d69809c6a13bffbdf90",
+    "url": "/bank-of-react/static/js/runtime-main.36cf4d5d.js"
+  }
+]);
